5174dc65a902c3b988e6c02d39409948e6531724
Peter Gissel <pgissel@airware.com>
2016-04-05 12:45:25 -0700

    add ifndef for boost futures to get rid of warnings on windows

--- autobahn/wamp_call.hpp
+++ autobahn/wamp_call.hpp
@@ -34,9 +34,11 @@
 #include "wamp_call_result.hpp"
 
 // http://stackoverflow.com/questions/22597948/using-boostfuture-with-then-continuations/
+#ifndef BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
 #define BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
+#endif
 #include <boost/thread/future.hpp>
 
 #include <msgpack.hpp>
--- autobahn/wamp_register_request.hpp
+++ autobahn/wamp_register_request.hpp
@@ -35,9 +35,11 @@
 #include "wamp_registration.hpp"
 
 // http://stackoverflow.com/questions/22597948/using-boostfuture-with-then-continuations/
+#ifndef BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
 #define BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
+#endif
 #include <boost/thread/future.hpp>
 
 namespace autobahn {
