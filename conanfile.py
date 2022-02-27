3f5d760972888c5ac7b82ade8beaa4209513450e
Alexander Arlt <alexander.arlt@arlt-labs.com>
2022-02-26 23:14:03 -0500

    Basic conan integration.

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,8 @@ class autobahn_cppConan(ConanFile):
     author = "Crossbar.io Technologies GmbH and contributors"
     description = "WAMP for C++ on Boost/ASIO"
     url = "https://github.com/crossbario/autobahn-cpp"
-    requires = "boost/1.73.0","msgpack/3.2.1","websocketpp/0.8.2"
-    generators = "cmake_find_package"
+    requires = "boost/1.77.0", "msgpack-cxx/4.0.3", "websocketpp/0.8.2", "openssl/3.0.1", "botan/2.19.1"
+    generators = "cmake"
     scm = {
         "type": "git",
         "subfolder": ".",
@@ -23,3 +23,4 @@ class autobahn_cppConan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
+
